212af1393182b48251a120d5ac8516d4f9403ffb
katielizinha <116743082+katielizinha@users.noreply.github.com>
2024-10-15 10:33:17 -0400

    update do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-type ProdutoType ={
-  id:number,
-  nome:string,
-  preco:string,
-  descricao:string,
-  imagem:string
+type ProdutoType = {
+    id:number,
+    nome:string,
+    preco:string,
+    descricao:string,
+    imagem:string
 }
-
 function App() {
   const [nome, setNome] = useState("")
-  const [produtos, setProdutos] = useState<ProdutoType[]>([])
-//useEffects(o que fazer, quando fazer ) []=> hora do carregamento da pagina 
-useEffect(()=>{
-  setNome("Katiély Fernanda Góis Santos ")
-  //buscar os dados do backEnd
-  fetch("https://one022a-marketplace-czsd.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(dados=>setProdutos(dados))
-},[])
-//Colocar em uma variavel 
+  const [produtos, setProdutos] = useState<ProdutoType []>([])
+  //useEffects(O que fazer, quando Fazer) []=> Hora do carregamento da página
+  useEffect(() => {
+    setNome("Guilherme Terenciani")
+    //Buscar os dados do BackENd
+    fetch("https://one022a-marketplace-e90o.onrender.com/produtos")
+      .then(resposta => resposta.json())
+      .then(dados => setProdutos(dados))
+    //Colocar em uma variável
+  }, [])
+
   return (
     <>
-    <h1>{nome}</h1>
-    <div className="produtos-container">
-      {
-        produtos.map(produto =>{
-          return(
-            <div className="produto-item">
-            <h1>{produto.nome}</h1>
-            <p>{produto.imagem}</p>
-            <p>{produto.preco}</p>
-            <p>{produto.descricao}</p>
-           </div>
-          )
-        })
-      }
-      
-    </div>
+      <h1>{nome}</h1>
+      <div className="produtos-container">
+        {
+          produtos.map(produto => {
+            return (
+              <div key={produto.id} className="produto-item">
+                <h1>{produto.nome}</h1>
+                <img src={produto.imagem} alt="Imagem do celular" />
+                <p>{produto.preco}</p>
+                <p>{produto.descricao}</p>
+              </div>
+            )
+          })
+        }
+
+      </div>
     </>
   )
 }
