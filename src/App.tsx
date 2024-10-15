f464213b2d57a4d1697ae5ad0833d47ccdaa47c9
katielizinha <116743082+katielizinha@users.noreply.github.com>
2024-10-15 11:48:57 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,9 @@ function App() {
             return (
               <div key={produto.id} className="produto-item">
                 <h1>{produto.nome}</h1>
+                <div className="container-imagem">
                 <img src={produto.imagem} alt="Imagem do celular" />
+                </div>
                 <p>{produto.preco}</p>
                 <p>{produto.descricao}</p>
               </div>
